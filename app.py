7835a0e3dd1910ffe28d619f0eaaf10768a0a752
clunny <miguellomcostta@icloud.com>
2025-11-05 16:34:50 +0000

    upd2

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route("/networks")
+def networks():
+    return render_template("networks.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
